af4b2663ec1a85de51d56863cac1a7a9d41b8716
hasardulmi <hasardulmi@gmail.com>
2024-09-17 23:03:55 +0530

    forget password page 1 created

--- app/src/main/java/com/example/goodeats9/Forget_Password.java
+++ app/src/main/java/com/example/goodeats9/Forget_Password.java
@@ -1,12 +1,16 @@
 package com.example.goodeats9;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class Forget_Password extends AppCompatActivity {
 
