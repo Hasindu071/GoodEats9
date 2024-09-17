c17804d1317900ac0d0e2ae3fd879870b7317508
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2024-09-17 22:00:30 +0530

    minor edits

--- app/src/main/java/com/example/goodeats9/login.java
+++ app/src/main/java/com/example/goodeats9/login.java
@@ -16,13 +16,6 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);  // Make sure to use the correct layout file
 
-        // Apply edge-to-edge window insets
-        View mainView = findViewById(R.id.main);
-        ViewCompat.setOnApplyWindowInsetsListener(mainView, (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return WindowInsetsCompat.CONSUMED;
-        });
 
         // Handle Sign Up TextView click
         findViewById(R.id.textViewSignUp).setOnClickListener(v -> {
