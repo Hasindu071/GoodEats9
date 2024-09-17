d68e7edb9b41620d7f157893d1c4c81914fd7354
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2024-09-17 16:25:39 +0530

    linked edit profile

--- app/src/main/java/com/example/goodeats9/profileFragment.java
+++ app/src/main/java/com/example/goodeats9/profileFragment.java
@@ -1,5 +1,6 @@
 package com.example.goodeats9;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +8,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 public class profileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        // Find the icon by ID
+        ImageView profileIcon = view.findViewById(R.id.editProfileButton);
+        profileIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click event (show edit profile activity)
+
+                Intent intent = new Intent(getActivity(), edit_profile.class);
+                startActivity(intent);
+
+            }
+        });
+
+        return view;
     }
 }
