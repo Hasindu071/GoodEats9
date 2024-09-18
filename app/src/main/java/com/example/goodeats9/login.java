5044d278d7e2f4176546215ffb6bc5558364ab78
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2024-09-18 10:23:06 +0530

    sign up link edited

--- app/src/main/java/com/example/goodeats9/login.java
+++ app/src/main/java/com/example/goodeats9/login.java
@@ -1,8 +1,10 @@
 package com.example.goodeats9;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -16,9 +18,12 @@ public class login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);  // Make sure to use the correct layout file
 
-
+        //Get the text of sign up by its ID
+        TextView textViewSignUp = findViewById(R.id.textViewSignUp);
+        //Make the "sign up" text look like a link
+        textViewSignUp.setPaintFlags(textViewSignUp.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         // Handle Sign Up TextView click
-        findViewById(R.id.textViewSignUp).setOnClickListener(v -> {
+        textViewSignUp.setOnClickListener(v -> {
             // Navigate to SignUpActivity when the Sign Up TextView is clicked
             Intent intent = new Intent(login.this, sign_up.class);
             startActivity(intent);
