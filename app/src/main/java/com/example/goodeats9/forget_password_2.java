45ce53cfa336a34b2f0c472a84686a61330dc573
hasardulmi <hasardulmi@gmail.com>
2024-09-20 15:13:52 +0530

    forget password 2 exit button created

--- app/src/main/java/com/example/goodeats9/forget_password_2.java
+++ app/src/main/java/com/example/goodeats9/forget_password_2.java
@@ -1,6 +1,10 @@
 package com.example.goodeats9;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class forget_password_2 extends AppCompatActivity {
+
+    public ImageButton extBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,18 @@ public class forget_password_2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        extBtn = findViewById(R.id.exitButton);
+        extBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the LoginActivity
+                Intent intent = new Intent(forget_password_2.this, login.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear back stack
+                startActivity(intent);
+                finish(); // Close the current activity
+            }
         });
     }
 }
