86c60536c52845d8462c44a00df0f8165a113510
hasardulmi <hasardulmi@gmail.com>
2024-09-20 18:42:06 +0530

    forget password 3 reset button completed

--- app/src/main/java/com/example/goodeats9/forget_password_3.java
+++ app/src/main/java/com/example/goodeats9/forget_password_3.java
@@ -1,6 +1,9 @@
 package com.example.goodeats9;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class forget_password_3 extends AppCompatActivity {
+
+    public Button Reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,20 @@ public class forget_password_3 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Reset = findViewById(R.id.buttonReset);
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navigate to the LoginActivity
+                Intent intent = new Intent(forget_password_3.this, forget_password_4.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear back stack
+                startActivity(intent);
+                finish();// Close the current activity
+            }
+        });
+
+
+
     }
 }
