4e67ac6d6dcd9e8e63e87537331fe955a0679623
hasardulmi <hasardulmi@gmail.com>
2024-09-20 20:45:20 +0530

    forget password 4 reset button is completed

--- app/src/main/java/com/example/goodeats9/forget_password_4.java
+++ app/src/main/java/com/example/goodeats9/forget_password_4.java
@@ -1,6 +1,9 @@
 package com.example.goodeats9;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class forget_password_4 extends AppCompatActivity {
+
+    public Button RstBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,20 @@ public class forget_password_4 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        RstBtn = findViewById(R.id.RstBtn);
+        RstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to the LoginActivity
+                Intent intent = new Intent(forget_password_4.this, login.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear back stack
+                startActivity(intent);
+                finish(); // Close the current activity
+            }
+        });
+
+
+
     }
 }
