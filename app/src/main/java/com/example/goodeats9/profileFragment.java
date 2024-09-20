e86c32479b34051642b7a6dac21b1438a3a2f11f
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2024-09-20 10:58:58 +0530

    minor changes

--- app/src/main/java/com/example/goodeats9/profileFragment.java
+++ app/src/main/java/com/example/goodeats9/profileFragment.java
@@ -1,5 +1,6 @@
 package com.example.goodeats9;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
@@ -26,6 +28,16 @@ public class profileFragment extends Fragment {
                 Intent intent = new Intent(getActivity(), edit_profile.class);
                 startActivity(intent);
 
+            }
+        });
+
+        Button viewRecipe = view.findViewById(R.id.viewRecipeButton);
+        viewRecipe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the click event (show recipe activity)
+                Intent intent = new Intent(getActivity(), recipeMain.class);
+                startActivity(intent);
             }
         });
 
