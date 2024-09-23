294313e70610ed70667345ce672b0e0b29b6df8d
TharuHiru <hirusha-im21011@stu.kln.ac.lk>
2024-09-23 18:24:01 +0530

    get the logged username and display

--- app/src/main/java/com/example/goodeats9/homeFragment.java
+++ app/src/main/java/com/example/goodeats9/homeFragment.java
@@ -1,19 +1,38 @@
 package com.example.goodeats9;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class homeFragment extends Fragment {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class homeFragment extends Fragment
+{
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Access SharedPreferences
+        SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("loginDetails", MODE_PRIVATE);
+        String username = sharedPreferences.getString("UserName", "User"); //If no name is fount type user
+        TextView welcomeTextView = view.findViewById(R.id.text_name); // get the textview ID
+        welcomeTextView.setText(username);//Set the name
     }
 }
