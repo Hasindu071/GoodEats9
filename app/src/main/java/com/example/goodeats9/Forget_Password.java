78ed4294ef20e4e1477b30dc0072a3585cbce4ab
hasardulmi <hasardulmi@gmail.com>
2024-09-23 18:54:03 +0530

    modified the reset button in forget password 1

--- app/src/main/java/com/example/goodeats9/Forget_Password.java
+++ app/src/main/java/com/example/goodeats9/Forget_Password.java
@@ -54,7 +54,7 @@ public class Forget_Password extends AppCompatActivity {
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         if (dataSnapshot.exists()) { //remove the current email
                             Toast.makeText(Forget_Password.this, "Please Check Your Email", Toast.LENGTH_SHORT).show(); //send message to the email and show the message
-                            startActivity(new Intent(Forget_Password.this, forget_password_2.class)); // back to forget password 2 page
+                            startActivity(new Intent(Forget_Password.this, forget_password_2.class)); // forward to forget password 2 page
                         } else {
                             editTextEmailAddress.setError("No account with this email"); // show the error message
                         }
