d9d38a010e4cd912e511082cc6590d921feeb81b
hasardulmi <hasardulmi@gmail.com>
2024-10-03 13:39:30 +0530

    new Datacls file is created

---
+++ app/src/main/java/com/example/goodeats9/Datacls.java
@@ -0,0 +1,63 @@
+package com.example.goodeats9;
+
+public class Datacls {
+    private String videoUri;
+    private String name;
+    private String description;
+    private String userName;
+    private String imageUri;
+
+
+    public Datacls() {
+        // Default constructor required for calls to DataSnapshot.getValue(Datacls.class)
+    }
+
+    public Datacls(String videoUri, String name, String description, String userName) {
+        this.videoUri = videoUri;
+        this.name = name;
+        this.description = description;
+        this.userName = userName;
+        this.imageUri = imageUri;
+    }
+
+    // Getters and Setters
+    public String getVideoUri() {
+        return videoUri;
+    }
+
+    public void setVideoUri(String videoUri) {
+        this.videoUri = videoUri;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageURL) {
+        this.imageUri = imageURL;
+    }
+}
